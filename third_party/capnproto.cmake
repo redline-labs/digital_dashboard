7f2fc0fb3fbd6666bef8841412747e7acee1b137
Ryan David <ryandavid773@gmail.com>
2025-07-24 21:36:57 -0700

    Add capnproto

---
+++ third_party/capnproto.cmake
@@ -0,0 +1,28 @@
+# Fetch capnproto
+FetchContent_Declare(
+    capnproto
+    GIT_REPOSITORY https://github.com/capnproto/capnproto.git
+    GIT_TAG 688ba65
+    GIT_SHALLOW TRUE
+)
+
+# Configure capnproto options before making it available
+# Disable building tests, examples, and tools we don't need
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+set(CAPNP_LITE OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(capnproto)
+
+# Copy capnproto license
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/licenses/capnproto)
+file(COPY ${capnproto_SOURCE_DIR}/LICENSE ${capnproto_SOURCE_DIR}/README.md
+     DESTINATION ${CMAKE_BINARY_DIR}/licenses/capnproto)
+
+# Write version info
+file(WRITE ${CMAKE_BINARY_DIR}/licenses/capnproto/fetch_info.txt
+"Library: capnproto
+Repository: https://github.com/capnproto/capnproto.git
+Tag/Version: 688ba65
+Shallow Clone: TRUE
+Patches Applied: None
+") 
